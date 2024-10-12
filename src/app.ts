a1b29326490a683e7ef6a0e8e926de173928b3ba
ZRMYDYCG <547471919@qq.com>
2024-10-12 12:27:32 +0800

    feat: 初始化项目

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+/*
+ * @Author: ZRMYDYCG
+ * @Date: 2024-10
+ * @LastEditors: ZRMYDYCG
+ * @LastEditTime: 2024-10
+ * @Description: app.ts
+ */
+// 引入koa
+import Koa from 'koa'
+import http from 'http'
+
+// 创建koa实例
+const app = new Koa()
+
+// 创建服务器
+const server: http.Server = new http.Server(app.callback())
+
+// 中间件
+app.use(async (ctx) => {
+  ctx.body = 'Hello World'
+})
+
+// 监听端口
+app.listen(9000, () => {
+  console.log('run success')
+  console.log('app started at port 9000...')
+})
